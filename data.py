9e5a245c8d1d8319b540c5d83c81e6cd8c642080
Yong Choi <yong.s.choi2@gmail.com>
2023-09-30 02:47:59 -0500

    Initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+import responses
+import requests
+import random
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+    "category": 18,
+}
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+question_data = data["results"]
+
